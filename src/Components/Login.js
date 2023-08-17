212999687498d5dc6194b789943295609412bfba
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2023-08-17 12:45:51 +0530

    Added: Some styles

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -28,47 +28,49 @@ const Login = () => {
           alert("Logged in Succefully");
           window.localStorage.setItem("token", data.data);
           window.location.href = "/user";
-        }else{
+        } else {
           alert("Please check the credentials");
         }
       });
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="p-5 m-5">
-        <h3 className="text-center ">Sign In - Login To Explore</h3>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="p-5 m-5">
+          <h3 className="text-center ">Sign In - Login To Explore</h3>
 
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
+          <div className="mb-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right">
+            Forgot <Link to="/password-reset">password?</Link>
+          </p>
         </div>
-        <p className="forgot-password text-right">
-          Forgot <Link to="/password-reset">password?</Link>
-        </p>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
